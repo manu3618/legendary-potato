98f42d4cd1184d6d7794e09e5d7d5dad51759610
manu3618 <manu+github@hbrt.eu>
2017-11-25 12:35:35 +0100

    [TESTS] Add tests for optional kernels

---
+++ tests/complex_samples.py
@@ -0,0 +1,13 @@
+"""
+Data used to test complex kernels
+"""
+import math
+
+import legendary_potato.kernel
+
+KERNEL_SAMPLES = {
+        legendary_potato.kernel.l2: [
+            math.sin, math.cos, lambda x: 1, lambda x: 0, lambda x: -1,
+            lambda x: x, lambda x: abs(x),
+                                    ],
+}
