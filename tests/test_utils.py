daa3f42cb7b1f0646648ac558737f2421d770e76
manu3618 <manu+github@hbrt.eu>
2017-11-29 15:17:18 +0100

    [TEST] Reorganize files
    Other test are being writen that need to share some utilities with other
    tests. Those are now placed in common.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,25 +10,11 @@ import legendary_potato.composition
 import legendary_potato.kernel
 import legendary_potato.utils
 
-TEST_PATH = os.path.join(os.path.abspath(os.path.curdir))
-SAMPLE_PATH = os.path.join(TEST_PATH, 'sample')
+from .common import TEST_PATH, kernel_sample_iterator
+
+
 GRAMMATRIX_PATH = os.path.join(TEST_PATH, 'gram_matrix')
 COMPOSITION_FILE = os.path.join(TEST_PATH, 'composition.yaml')
-
-
-def kernel_sample_iterator():
-    """Return an iterator over (kernels, samples).
-    """
-    for kern in kernel_iterator():
-        kern_name = kern.__name__
-        yield kern, kernel_samples(kern_name)
-
-
-def kernel_iterator():
-    """Return an iterator over kernels to be tested.
-    """
-    for kern_name in os.listdir(SAMPLE_PATH):
-        yield legendary_potato.kernel.__dict__.get(kern_name)
 
 
 def composition_iterator(filename=COMPOSITION_FILE):
@@ -37,48 +23,6 @@ def composition_iterator(filename=COMPOSITION_FILE):
     with open(filename, 'r') as fd:
         ret = list(yaml.load_all(fd))
     return ret[0].items()
-
-
-def kernel_samples(kernel_name):
-    """Return iterator over samples for a kernel from specific file(s).
-
-    The iterator generate (sample_name, sample)
-
-    A kernel_path is generated. If it is a file, each line is considered
-    to be a sample, and its name is the line number. If it is a direcctory,
-    each file is considered to be a string sample and its name is the
-    file name.
-    """
-    kernel_sample_path = os.path.join(SAMPLE_PATH, kernel_name)
-    sep = ','
-    if os.path.isfile(kernel_sample_path):
-        # One sample per line
-        with open(kernel_sample_path, 'r') as sample_file:
-            line = sample_file.readline()
-            try:
-                if len(np.fromstring(line, sep=sep)) > 0:
-                    # line composed of numbers
-                    is_string = False
-                else:
-                    # line composed of stings
-                    is_string = True
-            except ValueError:
-                # line composed of mix of strings and numbers. should be
-                # treated as strings
-                is_string = True
-            sample_file.seek(0)
-
-            for nu, line in enumerate(sample_file):
-                if is_string:
-                    yield (nu, [row.strip for row in line.split(sep)])
-                else:
-                    yield (nu, np.fromstring(line, sep=sep))
-    else:
-        # kernel_sample_path is a directory
-        for sample_file in os.listdir(kernel_sample_path):
-            file_path = os.path.join(kernel_sample_path, sample_file)
-            with open(file_path, 'r') as pot:
-                yield sample_file, pot.read()
 
 
 @pytest.mark.parametrize(('kernel', 'sample'), kernel_sample_iterator())
