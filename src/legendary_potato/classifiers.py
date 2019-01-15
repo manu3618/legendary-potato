[SVDD] Fix distance computation

src/legendary_potato/classifiers.py
@@ -8,7 +8,7 @@ import numpy as np
 import pandas as pd
 from scipy.optimize import minimize
 from sklearn.base import BaseEstimator, ClassifierMixin
-from sklearn.utils.validation import check_array, check_is_fitted, check_X_y
+from sklearn.utils.validation import check_is_fitted, check_X_y
 
 from .methods import KernelMethod
 
@@ -54,7 +54,9 @@ class SVDD(BaseEstimator, ClassifierMixin, KernelMethod):
         is_kernel_matrix -- if true, the input is  treated as a kernel matrix
         *args, **kwargs -- extra arguments for kernel function
         """
-        X, y = check_X_y(X, y)
+        # X, y = check_X_y(X, y) # TODO: add check method for X
+        n = len(y)
+        _, y = check_X_y(np.identity(n), y)
         self.X_ = X
         if C is not None:
             self.C = C
@@ -102,27 +104,40 @@ class SVDD(BaseEstimator, ClassifierMixin, KernelMethod):
         """Predict classes
 
         Args:
-            X: training sample
+            X (array like): list of test samples.
             decision_radius (numeric): modification of decision radius.
         The frontier between classes will be the computed hypersphere whose
         radius is multiply by this factor.
         """
-        # TODO # XXX
         check_is_fitted(self, ["X_", "alphas_"])
-        X = check_array(X)
+        # X = check_array(X)
 
         if self.hypersphere_nb == 1:
             return self._predict_one_hypersphere(X, decision_radius)
 
         else:
             # check class
-            dist_classes = pd.DataFrame(
-                {
-                    cl: svdd._dist_center(X)
-                    for cl, svdd in self.individual_svdd.items()
-                }
-            )
+            dist_classes = self.relative_dist_all_centers(X)
             return np.array(dist_classes.idxmin(axis=1))
+
+    def fit_predict(
+        self,
+        X,
+        y,
+        C=None,
+        kernel=None,
+        is_kernel_matrix=False,
+        *args,
+        **kwargs
+    ):
+        """Fit as the fit() methods.
+
+        Return:
+            (array) : class for each training sample.
+        """
+        self.fit(X, y, C, kernel, is_kernel_matrix, *args, **kwargs)
+        # TODO
+        raise NotImplementedError
 
     def _predict_one_hypersphere(self, X=None, decision_radius=1):
         """Compute results for one hypersphere
@@ -135,11 +150,20 @@ class SVDD(BaseEstimator, ClassifierMixin, KernelMethod):
         Returns:
             (np.array)
         """
-        pred = [self._dist_center(X) * decision_radius / self.radius_ - 1]
+        pred = self._dist_center(X) * decision_radius / self.radius_ - 1
         return np.sign(pred).reshape(-1)
 
-    def _dist_center(self, X):
-        """Compute ditance to classe center
+    def _dist_center(self, X=None):
+        """Compute ditance to class center.
+
+        Args:
+            X (array-like): list of input vectors. If None, use the train set.
+
+        Distance to center:
+        .. math::
+            \| z - c \|^2 = \|z\|^2 - 2 K(z, c) + \|c\|^2
+
+            c = \sum_t  \alpha_t \phi(X_t)
         """
         if not self.hypersphere_nb == 1:
             raise RuntimeWarning("Not available for multiclass SVDD")
@@ -148,41 +172,46 @@ class SVDD(BaseEstimator, ClassifierMixin, KernelMethod):
         dim = len(self.alphas_)
         if X is None:
             # return distances for training set
-            dists = [
-                self.kernel_matrix(i, i)
+            square_dists = [
+                np.power(self.kernel_matrix[i, i], 2)
                 - 2
                 * sum(
-                    self.alphas_[t] * self.kernel_matrix(i, t)
+                    self.alphas_[t] * self.kernel_matrix[i, t]
                     for t in range(dim)
                 )
-                + sum(
-                    self.alphas_[t]
-                    * self.alphas_[s]
-                    * self.kernel_matrix[s, t]
-                    for s, t in product(range(dim), range(dim))
+                + np.power(
+                    sum(
+                        self.alphas_[t]
+                        * self.alphas_[s]
+                        * self.kernel_matrix[s, t]
+                        for s, t in product(range(dim), range(dim))
+                    ),
+                    2,
                 )
                 for i in range(dim)
             ]
         else:
             # return distances for vector X
-            dists = [
-                self.kernel(z, z)
+            square_dists = [
+                np.power(self.kernel(z, z), 2)
                 - 2
                 * sum(
                     self.alphas_[t] * self.kernel(self.X_[t], z)
                     for t in range(dim)
                 )
-                + sum(
-                    self.alphas_[s]
-                    * self.alphas_[t]
-                    * self.kernel(self.X_[t], z)
-                    for s in range(dim)
-                    for t in range(dim)
+                + np.power(
+                    sum(
+                        self.alphas_[s]
+                        * self.alphas_[t]
+                        * self.kernel(self.X_[t], self.X_[s])
+                        for s, t in product(range(dim), range(dim))
+                    ),
+                    2,
                 )
                 for z in X
             ]
 
-        return dists
+        return np.sqrt(square_dists)
 
     def _fit_one_hypersphere(self, y=None, class1=1, class2=-1):
         """Perform actual fit process
@@ -231,14 +260,14 @@ class SVDD(BaseEstimator, ClassifierMixin, KernelMethod):
         # mean distance to support vectors
         radius = np.mean(
             [
-                self.dist_to_center(r, alphas)
+                self.dist_center_training_sample(r, alphas)
                 for r in range(dim)
                 if alphas[r] < C and not np.isclose(0, alphas[r])
             ]
         )
         return radius, alphas
 
-    def multiclass_dist_center(self, X=None):
+    def dist_all_centers(self, X=None):
         """Return distance to each class center.
         """
         if self.hypersphere_nb > 1:
@@ -250,7 +279,19 @@ class SVDD(BaseEstimator, ClassifierMixin, KernelMethod):
             dist_classes = {1: self._dist_center(X)}
         return pd.DataFrame(dist_classes)
 
-    def dist_to_center(self, r, alphas=None, cl=None):
+    def relative_dist_all_centers(self, X=None):
+        """Distane to all centers divided by class radius.
+        """
+        if self.hypersphere_nb > 1:
+            dist_classes = {
+                cl: svdd._dist_center(X) / svdd.radius_
+                for cl, svdd in self.individual_svdd.items()
+            }
+        else:
+            dist_classes = {1: self._dist_center(X)/self.radius_}
+        return pd.DataFrame(dist_classes)
+
+    def dist_center_training_sample(self, r, alphas=None, cl=None):
         """Distance from vector #r to center.
 
         Args:

tests/test_classifier.py
@@ -124,8 +124,14 @@ def test_svdd(dataset):
     svdd = classifiers.SVDD()
     svdd.fit(X_train, y_train)
 
-    for X in X_train, X_test:
-        assert np.all(svdd.multiclass_dist_center(X) >= 0)
+    assert svdd.radius_ > 0
+    assert np.all(
+        svdd.dist_center_training_sample(r) >= 0 for r in range(len(X_train))
+    )
+
+    for X in X_train, X_test, None:
+        assert np.all(svdd.dist_all_centers(X) >= 0)
+        assert np.all(svdd.relative_dist_all_centers(X) >= 0)
 
 
 @pytest.mark.parametrize("classifier", classifier_iterator())