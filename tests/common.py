3451fe0aca0bd77b3d59394c531a53eb68e03ad0
manu3618 <manu+github@hbrt.eu>
2019-01-22 18:42:54 +0100

    [TESTS] Move sample generation into common

--- tests/common.py
+++ tests/common.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import os
+from itertools import product
 
 import numpy as np
 
@@ -64,3 +65,69 @@ def kernel_samples(kernel_name):
             file_path = os.path.join(kernel_sample_path, sample_file)
             with open(file_path, "r") as pot:
                 yield sample_file, pot.read()
+
+
+def two_class_generator(random_state=1576, dims=(1, 10)):
+    """Generate classic multidimensional toy set.
+
+    For each label, a set of centers are generated following normal
+    distribution. A set of samples are generated following anormal distribution
+    centered on this centers.
+
+    The generated set is a 2-class balanced dataset.
+
+    dims -- tuple indicating smallest and highest dimension tests
+    """
+    np.random.seed = random_state
+    for dim in range(*dims):
+        orig = np.zeros(dim)
+        orig[0] = -1
+        a_centers = np.random.normal(orig, 2, size=(3, dim))
+        orig[0] = 1
+        b_centers = np.random.normal(orig, 2, size=(3, dim))
+        a_data = np.vstack(
+            [
+                np.random.normal(center, 0.5, size=(5, dim))
+                for center in a_centers
+            ]
+        )
+        b_data = np.vstack(
+            [
+                np.random.normal(center, 0.5, size=(5, dim))
+                for center in b_centers
+            ]
+        )
+
+        labels = [1 for _ in range(15)] + [-1 for _ in range(15)]
+        data = np.hstack([np.vstack([a_data, b_data]), np.transpose([labels])])
+        np.random.shuffle(data)
+        yield data[:, 0:-1], data[:, -1]
+
+
+def multiclass_generator(
+    random_state=1576, dims=(2, 3), nb_classes=(3, 5), cl_sample=5
+):
+    """Generate classic multiclass toyset.
+
+    Works as the two_class_generator but alternativly with number and string
+    labels.
+    """
+    np.random.seed = random_state
+    label_list = list("abcdefghijklmnopqrstuvwxyz")
+    for dim, nb_cl in product(range(*dims), range(*nb_classes)):
+        centers = np.random.normal(np.zeros(dim + 1), 5, size=(nb_cl, dim + 1))
+        datas = np.vstack(
+            [
+                np.random.normal(
+                    centers[cl, :], 0.5, size=(cl_sample, dim + 1)
+                )
+                for cl in range(nb_cl)
+            ]
+        )
+        datas[:, -1] = np.repeat(range(nb_cl), cl_sample)
+        np.random.shuffle(datas)
+        yield datas[:, 0:-1], datas[:, -1]
+        yield (
+            datas[:, 0:-1],
+            np.array([label_list[int(i)] for i in datas[:, -1]]),
+        )
